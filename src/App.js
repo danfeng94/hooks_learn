e8b924a789a9e2a65ac93b524981d46491feec22
Klay Yun <klay.yuan@songmao.tech>
2021-03-17 15:27:38 +0800

    feat: 新增hooks的钩子

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import MyMemo from "./components/myMemo";
+function App() {
+  return (
+    <div className="App">
+      <MyMemo />
+    </div>
+  );
+}
+
+export default App;
